ca06cd0274a0a8fe12fe5c8bb78792f9bff55851
jaemoon <woans5970@naver.com>
2023-12-19 10:48:32 +0900

    section 4: 생성자 - 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,13 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); // 생성자 호출 * 항상 첫 줄에 적어야됨 첫 줄이 아닌 경우 오류 발생
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name: " + name + ", age: " + age + ", grade: " + grade);
         this.name = name;
