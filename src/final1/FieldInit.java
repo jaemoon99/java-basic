253c2bc5110196c1fe3f951e483dfca64bfa59c8
jaemoon <woans5970@naver.com>
2023-12-19 22:54:02 +0900

    section 8: final 변수와 상수 1

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10; // 상수, 상수는 모두 대문자
+    final int value = 10;
+
+//    public FieldInit(int value) {
+//        this.value = value; // 초기 값이 있는 경우 변경 불가.
+//    }
+}
